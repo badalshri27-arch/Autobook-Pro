4b247a8bd6c7e1ee99c7bf8fa243013e07aa8a8c
Badal <badalshri27@gmail.com>
2026-05-16 17:47:52 +0530

    fix: cors for extension

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,12 @@ const Admin = require("./models/Admin")
 
 const app = express()
 
-app.use(cors())
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}))
+app.options('*', cors())
 app.use(express.json())
 
 mongoose.connect(process.env.MONGO_URI)
